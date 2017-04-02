b2c9262c072c742f6e78d39abae52d3157a73305
andrasat <andra.satria1@gmail.com>
2017-04-03 02:56:21 +0700

    add, checked, and delete button done

--- scripts/script.js
+++ scripts/script.js
@@ -3,80 +3,27 @@ $(document).ready(function() {
 // Add close button on each LI elements
   (function () {
     let close = $('<span class="close"></span>').text('\u00D7')
-    console.log($('li'))
-    $('li').append(close)
+    $('#todo-list').children().append(close)
   }())
 
-// Add click event handler on close button to remove each LI clicked
-  $('.close').on('click', (e)=> {
-    console.log(e.target)
+// '.addBtn' add new todo list
+  $('.addBtn').on('click', ()=> {
+    let userInput = $('#todo-input').val()
+    if(!userInput){
+      alert('Please input something')
+    } else {
+      $('#todo-list').append(`<li>${userInput}<span class="close">\u00D7</span></li>`)
+    }
+  })
+
+// '.close' click event handler on close button to remove each LI clicked
+  $('#todo-list').on('click', '.close', (e)=> {
     $(e.target).parent().remove()
   })
 
-  function newElement() {
-    let userInput = $('#todo').val()
-
-    if(!userInput) {
-      alert('You must write something !')
-    } else {
-      $('#todo-list').append()
-    }
-  }
+// 'li' click event handler on li element and make checked list
+  $('#todo-list').on('click', 'li', (e)=> {
+    $(e.target).toggleClass('checked')
+  })
 
 })
-
-// // Create a "close" button and append it to each list item
-// var myNodelist = document.getElementsByTagName("LI");
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
-//
-// // Click on a close button to hide the current list item
-// var close = document.getElementsByClassName("close");
-// var i;
-// for (i = 0; i < close.length; i++) {
-//   close[i].onclick = function() {
-//     var div = this.parentElement;
-//     div.style.display = "none";
-//   }
-// }
-//
-// // Add a "checked" symbol when clicking on a list item
-// var list = document.querySelector('ul');
-// list.addEventListener('click', function(ev) {
-//   if (ev.target.tagName === 'LI') {
-//     ev.target.classList.toggle('checked');
-//   }
-// }, false);
-//
-// // Create a new list item when clicking on the "Add" button
-// function newElement() {
-//   var li = document.createElement("li");
-//   var inputValue = document.getElementById("todo").value;
-//   var t = document.createTextNode(inputValue);
-//   li.appendChild(t);
-//   if (inputValue === '') {
-//     alert("You must write something!");
-//   } else {
-//     document.getElementById("todo-list").appendChild(li);
-//   }
-//   document.getElementById("todo").value = "";
-//
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   li.appendChild(span);
-//
-//   for (i = 0; i < close.length; i++) {
-//     close[i].onclick = function() {
-//       var div = this.parentElement;
-//       div.style.display = "none";
-//     }
-//   }
-// }
